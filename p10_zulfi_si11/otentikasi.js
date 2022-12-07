2ba3aacb90c103f4947358c97442e383c1f24abc
zulfiabdillah19 <114558410+zulfiabdillah19@users.noreply.github.com>
2022-12-07 12:46:56 +0700

    pertemuan ke 10

---
+++ p10_zulfi_si11/otentikasi.js
@@ -0,0 +1,12 @@
+function kirim() {
+    let inp_nama = document.getElementById("username").value;
+    let inp_pass = document.getElementById("password").value;
+    let form = document.getElementById("form");
+
+    if (inp_nama == "admin" && inp_pass == "integrity") {
+        form.setAttribute("action","dashboard.html")
+        alert("login berhasil"); 
+    }else{
+        alert("login gagal");
+    }
+}
